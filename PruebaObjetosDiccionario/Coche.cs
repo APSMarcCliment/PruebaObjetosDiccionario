cc75b917ce5fef0d25502f7ecc676dca14f1415b
APSMarcCliment <marc.climent@airplane.solutions>
2022-12-02 11:09:50 +0100

    First commit

---
+++ PruebaObjetosDiccionario/Coche.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+namespace PruebaObjetosDiccionario
+{
+	public class Coche
+	{
+		private int velocidad = 0;
+		private string marca = "";
+		private int matricula = 0;
+		private string color = "";
+		private bool estaParado = false;
+		private int orientacion = 0;/// <summary>
+									/// 0-arriba  1-abajo  2-izquierda  3-derecha
+									/// </summary>
+
+		public Coche(string marca, int matricula, int velocidad, string color, bool estaParado, int orientacion)
+		{
+			this.marca = marca;
+			this.matricula = matricula;
+			this.color = color;
+            this.velocidad = velocidad;
+            this.estaParado = false;
+			this.orientacion = orientacion;
+		}
+		public Coche()
+		{
+
+		}
+
+		public int getMatricula() { return matricula; }
+		public string getColor() { return color; }
+		public string getMarca() { return marca; }
+		public int getVelocidad() { return velocidad; }
+		public bool getEstaParado() { return estaParado; }
+		public int getOrientacion() { return orientacion; }
+		public void setColor(string color)
+		{
+			this.setColor(color);
+		}
+		public void setMatricula(int matricula)
+		{
+			this.setMatricula(matricula);
+		}
+		public void setMarca(string marca)
+		{
+			this.setMarca(marca);
+		}
+		public void setVelocidad(int velocidad)
+		{
+			this.setVelocidad(velocidad);
+		}
+		public void setEstaParado(bool estaParado)
+		{
+			this.setEstaParado(estaParado);
+		}
+		public void setOrientacion(int orientacion)
+		{
+			this.setOrientacion(orientacion);
+		}
+	}
+
+}
---
+++ PruebaObjetosDiccionario/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaObjetosDiccionario
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Coche coche1 = new Coche("Ferrari", 1234, 120, "Rosa", false, 1);
+            Coche coche2 = new Coche("Alpine", 7436, 130, "Azul", true, 2);
+            Coche coche3 = new Coche("Mclaren", 8473, 110, "Verde", true, 3);
+            Coche coche4 = new Coche("Mercedes", 0294, 100, "Amarillo", false, 1);
+
+            List<Coche> coches = new List<Coche>();
+            coches.Add(coche1);
+            coches.Add(coche2);
+            coches.Add(coche3);
+            coches.Add(coche4);
+            ///coches.Add(null);
+            
+            try
+            {
+                Dictionary<int, Coche> carList = new Dictionary<int, Coche>();
+                for(int i = 0; i < coches.Count; i++)
+                {
+                    carList.Add(coches[i].getMatricula(), coches[i]);
+                }
+                int miClave = (int)carList.Keys.ElementAt(0);
+
+                Console.WriteLine(miClave);
+                Coche miCoche = carList[miClave];
+                miCoche.getColor();
+                int opcion = 0;
+                string colorCoche;
+                string marcaCoche;
+                string colorFinal;
+                Console.WriteLine("1.Buscar coche por color");
+                Console.WriteLine("2.Buscar coche por marca");
+                Console.WriteLine("3.Cambiar color");
+                Console.WriteLine("4.Salir");
+                while (opcion != 4)
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            colorCoche = Console.ReadLine();
+                            if (miCoche.getColor().Equals(colorCoche))
+                            {
+                                Console.WriteLine(miCoche.getMatricula());
+                            }
+                            else
+                            {
+                                Console.WriteLine("No hay coches con este color");
+                            }
+                            break;
+                        case 2:
+                            marcaCoche = Console.ReadLine();
+                            if (miCoche.getColor().Equals(marcaCoche))
+                            {
+                                Console.WriteLine(miCoche.getMatricula());
+                            }
+                            else
+                            {
+                                Console.WriteLine("No hay coches de esta marca");
+                            }
+                            break;
+                        case 3:
+                            colorCoche = Console.ReadLine();
+                            Console.WriteLine("A que color lo quieres cambiar?");
+                            colorFinal = Console.ReadLine();
+                            if (miCoche.getColor().Equals(colorCoche))
+                            {
+                                miCoche.setColor(colorFinal);
+                            }
+                            else
+                            {
+                                Console.WriteLine("No hay coches con este color");
+                            }
+                            break;
+                        case 4:
+                            Console.WriteLine("Saliendo de las opciones");
+                            break;
+                        default:
+                            Console.WriteLine("No es una opcion valida");
+                            break;
+                    }
+                }  
+            }
+            catch(ArgumentNullException e)///no tiene ningun valor
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NullReferenceException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            finally { Console.WriteLine("Cerrar recuperar un coche"); }
+        }
+    }
+}
